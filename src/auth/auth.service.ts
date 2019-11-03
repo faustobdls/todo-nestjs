9de5ca81cf793b435d5884cd60817c13f0bd4c35
Jacob  Moura <jacobmoura@Jacobs-iMac.local>
2019-11-03 16:48:44 -0300

    remove user object from token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,39 +8,37 @@ export class AuthService {
   constructor(
     private readonly usersService: UsersService,
     private readonly jwtService: JwtService
-  ) {}
+  ) { }
 
   private async validate(email: string, password: string): Promise<Users> {
     return await this.usersService.findOne(email, password);
-}
+  }
 
-  public async login(user: Users): Promise< any | { status: number }>{
-    return this.validate(user.email, user.password).then((userData)=>{
-      if(!userData){
+  public async login(user: Users): Promise<any | { status: number }> {
+    return this.validate(user.email, user.password).then((userData) => {
+      if (!userData) {
         return { status: 404 };
       }
-      const {password, ...result} = userData; 
-      let payload = {...result};
+      const { password, ...result } = userData;
+      let payload = { ...result };
       const accessToken = this.jwtService.sign(payload, {
-        expiresIn: 3600
+        expiresIn: 3600,
       });
 
       return {
-         expires_in: 3600,
-         access_token: accessToken,
-         user: payload,
-         status: 200
+        expires_in: 3600,
+        access_token: accessToken,
       };
 
     });
-}
+  }
 
-public async register(user: Users): Promise<any>{
+  public async register(user: Users): Promise<any> {
     return this.usersService.create(user);
-}
+  }
 
-public user(jwttoken: string): any {
-  return this.usersService.findById(this.jwtService.decode(jwttoken)['id']);
-}
+  public user(jwttoken: string): any {
+    return this.usersService.findById(this.jwtService.decode(jwttoken)['id']);
+  }
 
 }
